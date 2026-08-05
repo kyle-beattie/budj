2e96f38ad03f824637856c39f00319370b0d8582
Kyle Beattie <kyle@thebeatties.co>
2026-08-05 15:39:50 +1200

    Initial Commit

---
+++ Budj/BudjApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudjApp.swift
+//  Budj
+//
+//  Created by Kyle Beattie on 05/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct BudjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Budj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Budj
+//
+//  Created by Kyle Beattie on 05/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
